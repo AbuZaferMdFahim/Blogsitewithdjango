3b6d68b01711fcaa69c3d4705c5d6858fc362b47
AbuZaferMdFahim <mightyanim007@gmail.com>
2023-05-13 02:20:04 +0600

    blog comment

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
-from .models import (Blog,Tag,Category)  
+from .models import Blog, Tag, Category, Comment, Reply
+from .forms import TextForm  
 
 def home(request):
     blogs = Blog.objects.order_by('-created')
@@ -64,11 +66,31 @@ def tag_blogs(request,slug):
     return render(request, 'tag_blogs.html',context)
 
 def blog_details(request,slug):
+    form = TextForm()
     blog = get_object_or_404(Blog, slug=slug)
     category = Category.objects.get(id = blog.category.id)
     related_blogs = category.category_blogs.all()
     tags = Tag.objects.order_by('created')[:4]
 
-    context = {"blog": blog, "related_blogs": related_blogs, "tags": tags}
+    if request.method == "POST" and request.user.is_authenticated:
+        form = TextForm(request.POST)
+        if form.is_valid():
+            Comment.objects.create(user = request.user, blog_comment = blog, text = form.cleaned_data.get('text'))
+            return redirect('blog_details', slug=slug)        
+
+    context = {"blog": blog, "related_blogs": related_blogs, "tags": tags, "form": form}
     return render(request, 'blog_details.html', context)
 
+@login_required(login_url='login')
+def add_reply(request, blog_id, comment_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    if request.method == "POST":
+        form = TextForm(request.POST)
+        if form.is_valid():
+            comment = get_object_or_404(Comment, id=comment_id)
+            Reply.objects.create(
+                user=request.user,
+                blog_reply=comment,
+                text=form.cleaned_data.get('text')
+            )
+    return redirect('blog_details', slug=blog.slug)
